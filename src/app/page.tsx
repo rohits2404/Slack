1b689608624d239d7cd4ea1961d785082fd46a6c
Rohit Sharma <rohits8731@gmail.com>
2025-08-15 21:44:26 +0530

    04: Github Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import { AuthScreen } from '@/features/auth/components/auth-screen'
+"use client";
+
 import React from 'react'
+import { Button } from '@/components/ui/button';
+import { useAuthActions } from '@convex-dev/auth/react';
 
 const Home = () => {
+
+    const {signOut} = useAuthActions();
+
     return (
-        <AuthScreen/>
+        <div>
+            Logged In
+            <Button onClick={() => signOut()}>
+                Sign Out
+            </Button>
+        </div>
     )
 }
 
---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import {
+  convexAuthNextjsMiddleware,
+  createRouteMatcher,
+  nextjsMiddlewareRedirect,
+} from "@convex-dev/auth/nextjs/server";
+ 
+const isSignInPage = createRouteMatcher(["/auth"]);
+ 
+export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+    if (!isSignInPage(request) && !(await convexAuth.isAuthenticated())) {
+        return nextjsMiddlewareRedirect(request, "/auth");
+    }
+    if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
+        return nextjsMiddlewareRedirect(request, "/");
+    }
+});
+ 
+export const config = {
+    // The following matcher runs middleware on all routes
+    // except static assets.
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
