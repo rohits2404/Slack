130778dc25c1825ce8ef8151481be797712da31b
Rohit Sharma <rohits8731@gmail.com>
2025-08-15 19:20:34 +0530

    01: Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='text-rose-500 font-bold text-2xl'>
+            Hello World
+        </div>
+    )
+}
+
+export default Home
